d208b7befe766b5ca2abd3b90064eba1f6ac009d
afrauenpreis <59623065+afrauenpreis@users.noreply.github.com>
2020-05-01 19:33:15 -0700

    more helper functions
    added code to get any region list, which means we can get ride of the all_countries function now cause its redundant. Also added code to get hotspots by region and lat and long values

---
+++ R/give_hotspots.R
@@ -0,0 +1,23 @@
+#' Returns dataframe of birding hotspots for a given region. Must enter the regions code for region, for example if you wanted hotspots
+#' in the United States you would enter "region = `US`".
+#'
+#' @param key The users eBird key
+#' @param region The region code for the desired region
+#' @param back How many days back to collect data, default is 1
+#'
+#' @return subregions datafram
+#'
+#' @importFrom httr GET add_headers
+#' @importFrom jsonlite FromJSON
+#' @importFrom glue glue
+#'
+#' @export
+give_hotspots <- function(region, back = 1, key) {
+  url <- glue("https://api.ebird.org/v2/ref/hotspot/{region}")
+  hotspots <- GET(url,
+                 add_headers("x-ebirdapitoken" = key),
+                 query = list(back = back,
+                              fmt = "json"))
+  hotspots <- fromJSON(rawToChar(hotspots$content))
+  hotspots
+}
---
+++ R/give_regions.R
@@ -0,0 +1,23 @@
+#' Returns dataframe of subregions and their codes for a parent country or a parent subregion
+#' Can also give list of all countries if region_type is set to "country"
+#'
+#' @param key The users eBird key
+#' @param region_type The options for this are country, subnational1, and subnational2
+#' @param parent_region The options for this are world (the default),
+#'
+#' @return subregions datafram
+#'
+#' @importFrom httr GET add_headers
+#' @importFrom jsonlite FromJSON
+#' @importFrom glue glue
+#'
+#' @export
+give_regions <- function(region_type, parent_region = "world", key) {
+  stopifnot(region_type %in% c("country", "subnational1", "subnational2"),
+            parent_region %in% c("world", "country", "subnational1"))
+  url <- glue("https://api.ebird.org/v2/ref/region/list/{{region_type}}/{parent_region}")
+  regions <- GET(url,
+                 add_headers("x-ebirdapitoken" = key))
+  regions <- fromJSON(rawToChar(regions$content))
+  regions
+}
