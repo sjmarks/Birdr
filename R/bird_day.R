4c2856e81d7757301083f3112840346f9540fc19
dylan12899 <46506909+dylan12899@users.noreply.github.com>
2020-05-02 17:29:12 -0700

    updated description

--- R/bird_day.R
+++ R/bird_day.R
@@ -1,13 +1,18 @@
-#' Returns dataframe of regions and the birds present on a given day
-#' Can also give list of all countries if region_type is set to "country"
+#' Creates a dataframe of birds spotted on a single day for a region (default United States)
+#' For specific states or counties use pick_UScode to generate the country ID. 
 #'
-#' @param country two letter country abreviation, default is United States
-#' @param year 4 digit year written as a string
-#' @param month month 1-12 written as a string
-#' @param day day 1-31 written as a string
+#' @description returns dataframe of all birds spotted on a single day for a region
+#'
+#' @param country string of country letter abreviation, default is United States
+#' @param year string of 4 digit year
+#' @param month string of month 1-12 
+#' @param day string of day 1-31 
 #' @param key The users eBird key
 #'
 #' @return bird_obs dataframe
+#'
+#' @examples
+#' bird_day <- function(year = "1999", month = "1", day = "28", key = my_birdkey)
 #'
 #' @importFrom httr GET add_headers
 #' @importFrom jsonlite fromJSON
@@ -24,5 +29,6 @@ bird_day <- function(country = "US", year, month, day, key ){
                   query = list(fmt = "json"))
   bird_obs <- fromJSON(rawToChar(bird_obs$content)) %>%
     drop_na()
-
+  
+return(bird_obs)
 }
